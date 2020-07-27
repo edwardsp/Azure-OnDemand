456933a5d20c8cf429005a6342c5e499e3aa7cbe
Cedric Husianycia <cehusian@microsoft.com>
2020-07-27 10:33:21 +0200

    root traffic from port 80 to 3000

--- azhpc-slurm/scripts/install-grafana.sh
+++ azhpc-slurm/scripts/install-grafana.sh
@@ -52,6 +52,8 @@ systemctl enable grafana-server
 #sudo firewall-cmd --permanent --zone=public --add-port=3000/tcp
 #echo "#### Reload firewall rules:"
 #sudo firewall-cmd --reload
+# echo "#### Root traffice from port 80 to port 3000"
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
 
 echo "#### Configuration of influxDB User and DB:"
 curl "http://localhost:8086/query" --data-urlencode "q=CREATE USER admindb WITH PASSWORD '$GRAFANA_PWD' WITH ALL PRIVILEGES"
