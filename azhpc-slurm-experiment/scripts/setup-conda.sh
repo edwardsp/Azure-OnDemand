7bda49d6586010e30b51a479b02c35d99abc95a0
Paul Edwards <paul.edwards@microsoft.com>
2020-07-29 15:24:52 +0100

    moved from azhpc-slurm

--- azhpc-slurm-experiment/scripts/setup-conda.sh
+++ azhpc-slurm-experiment/scripts/setup-conda.sh
@@ -4,5 +4,5 @@ export PATH=$PATH:/opt/anaconda/bin
 conda init
 conda config --set auto_activate_base true
 ln -s /opt/anaconda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
-conda create -n tensorflow_env tensorflow jupyter
+conda create -n tensorflow_env tensorflow tensorflow-gpu jupyter pip numpy pillow matplotlib
 echo "Conda setup complete"
