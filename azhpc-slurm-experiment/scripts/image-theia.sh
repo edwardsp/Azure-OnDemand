19768733a715cc9b8823ca69b7517cfed153872b
Xavier Pillons <xpillons@microsoft.com>
2020-07-29 19:27:25 +0200

    exit on error

--- azhpc-slurm-experiment/scripts/image-theia.sh
+++ azhpc-slurm-experiment/scripts/image-theia.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
+
 # install nodejs
 curl -sL https://rpm.nodesource.com/setup_10.x | bash -
-yum install -y nodejs
+yum install -y nodejs || exit 1
 
 # install yarn
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
 rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
-yum -y install yarn
+yum -y install yarn || exit 1
 
 # theia dependencies
 yum groupinstall -y "Development Tools"
-yum install -y git libX11-devel libxkbfile-devel
+yum install -y git libX11-devel libxkbfile-devel || exit 1
