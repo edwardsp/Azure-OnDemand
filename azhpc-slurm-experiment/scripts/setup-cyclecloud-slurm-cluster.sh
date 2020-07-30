1c186bbc26ece1d68c2df3a90b02e57ce128b667
Ben Watrous <ben.watrous@microsoft.com>
2020-07-29 19:05:59 -0700

    Create and start a headless Slurm cluster in CycleCloud.

---
+++ azhpc-slurm-experiment/scripts/setup-cyclecloud-slurm-cluster.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Add the cluster creation icon (for convenience)
+sudo /usr/local/bin/cyclecloud import_template Slurm-Headless -c slurm -f scripts/cyclecloud-slurm-headless.txt --force
+
+RESOURCE_GROUP=$1
+LOCATION=$2
+VNET_NAME=$3
+SUBNET_NAME=$4
+MAX_HPC_VM_COUNT=$5
+HPC_IMAGE_ID=$6
+HPC_MACHINE_TYPE=$7
+
+NFS_SERVER=$8
+NFS_EXPORT=${9-/share}
+if [ -z "$NFS_SERVER" ]; then
+    echo "The NFS server address is required"
+    exit 1
+fi
+
+# Account name currently hard-coded in setup-cyclecloud8.sh
+CC_ACCOUNT=azure
+SLURM_VERSION="19.05.5-1"
+#SUBNET_ID="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Network/virtualNetworks/${VNET_NAME}/subnets/${SUBNET_NAME}"
+
+cat <<EOF > scripts/azurehpcood-slurm.json
+{
+  "Credentials" : "${CC_ACCOUNT}",
+  "Autoscale" : true,
+  "SubnetId" : "${RESOURCE_GROUP}/${VNET_NAME}/${SUBNET_NAME}",
+  "slurm" : null,
+  "NFSAddress" : "${NFS_SERVER}",
+  "NFSExport" : "${NFS_EXPORT}",
+  "Region" : "${LOCATION}",
+  "MaxHPCExecuteCount" : ${MAX_HPC_VM_COUNT},
+  "HPCImageName" : "${HPC_IMAGE_ID}",
+  "HPCClusterInitSpecs" : null,
+  "configuration_slurm_version" : "${SLURM_VERSION}",
+  "HPCMachineType" : "${HPC_MACHINE_TYPE}",
+  "HPCMaxScalesetSize" : 100
+}
+EOF
+
+sudo /usr/local/bin/cyclecloud import_cluster azurehpcood-slurm -c slurm -f scripts/cyclecloud-slurm-headless.txt -p scripts/azurehpcood-slurm.json --force
+sudo /usr/local/bin/cyclecloud start_cluster azurehpcood-slurm
--- azhpc-slurm-experiment/scripts/setup-cyclecloud8.sh
+++ azhpc-slurm-experiment/scripts/setup-cyclecloud8.sh
@@ -32,20 +32,32 @@ if [ -z "${INITIAL_USER_PASSWORD}" ]; then
     # Use randomized passsword 
     INITIAL_USER_PASSWORD=$( tr -cd '[:alnum:]' < /dev/urandom | fold -w16 | head -n1 )
 fi
-if [ -f "~${INITIAL_USER_NAME}/.ssh/id_rsa.pub" ]; then
-    INITIAL_USER_PUBKEY=$( cat "~${INITIAL_USER_NAME}/.ssh/id_rsa.pub" )
+
+INITIAL_USER_PUBKEY_FILE=$6
+if [ -z "${INITIAL_USER_PUBKEY_FILE}" ]; then
+    if [ -f "~${INITIAL_USER_NAME}/.ssh/id_rsa.pub" ]; then
+        INITIAL_USER_PUBKEY=$( cat "~${INITIAL_USER_NAME}/.ssh/id_rsa.pub" )
+    fi
+else
+    INITIAL_USER_PUBKEY=$( cat "${INITIAL_USER_PUBKEY_FILE}" )
 fi
 
 AZURE_CLOUD="public"  # [china|germany|public|usgov]
 SUBSCRIPTION_ID=$( curl -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance/compute/subscriptionId?api-version=2019-06-01&format=text" )
+
+# Currently port 80 and 443 are reserved for OoD
+CYCLECLOUD_HTTP_PORT=8080
+CYCLECLOUD_HTTPS_PORT=8443
+iptables -I INPUT -p tcp -m tcp --dport ${CYCLECLOUD_HTTPS_PORT} -j ACCEPT
+iptables-save > /etc/sysconfig/iptables
 
 CS_ROOT=/opt/cycle_server
 $CS_ROOT/cycle_server stop
 
 # Update properties
 sed -i 's/webServerMaxHeapSize\=2048M/webServerMaxHeapSize\=4096M/' $CS_ROOT/config/cycle_server.properties
-sed -i 's/webServerPort\=8080/webServerPort\=80/' $CS_ROOT/config/cycle_server.properties
-sed -i 's/webServerSslPort\=8443/webServerSslPort\=443/' $CS_ROOT/config/cycle_server.properties
+sed -i "s/webServerPort\=8080/webServerPort\=${CYCLECLOUD_HTTP_PORT}/" $CS_ROOT/config/cycle_server.properties
+sed -i "s/webServerSslPort\=8443/webServerSslPort\=${CYCLECLOUD_HTTPS_PORT}/" $CS_ROOT/config/cycle_server.properties
 sed -i 's/webServerEnableHttps\=false/webServerEnableHttps=true/' $CS_ROOT/config/cycle_server.properties
 
 ls -l $CS_ROOT/components
@@ -101,13 +113,14 @@ fi
 $CS_ROOT/cycle_server start --wait
 
 # If public FQDN provided, we can use LetsEncrypt
-if  [ ! -z "${FQDN}" ] ; then
-  ${CS_ROOT}/cycle_server keystore automatic --accept-terms ${FQDN} 
-fi
+# TODO: we should figure out  a way to use letsEncrypt alongside OoD
+#if  [ ! -z "${FQDN}" ] ; then
+#  ${CS_ROOT}/cycle_server keystore automatic --accept-terms ${FQDN} 
+#fi
 
 # Configure the CLI for root as the initial user
 /usr/bin/sleep 5
-sudo /usr/local/bin/cyclecloud initialize --batch --url="https://localhost" --verify-ssl="false" \
+sudo /usr/local/bin/cyclecloud initialize --batch --url="https://localhost:${CYCLECLOUD_HTTPS_PORT}" --verify-ssl="false" \
                                      --username="${INITIAL_USER_NAME}" --password="${INITIAL_USER_PASSWORD}"
 
 # Configure the initial subscription
@@ -126,5 +139,11 @@ cat <<EOF > /tmp/azure_data.json
     "RMStorageContainer": "cyclecloud"
 }
 EOF
-sudo /usr/local/bin/cyclecloud account create -f /tmp/azure_data.json
 
+if /usr/local/bin/cyclecloud account show azure | grep -q 'Credentials: azure'; then
+    echo "Account \"azure\" already exists.   Skipping account setup..."
+else
+    sudo /usr/local/bin/cyclecloud account create -f /tmp/azure_data.json
+fi
+
+echo 'Azure CycleCloud Installation Complete'
