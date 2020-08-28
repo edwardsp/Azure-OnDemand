6a709ab7fceed1f2e4661727532f5710809f5d9b
Paul Edwards <paul.edwards@microsoft.com>
2020-08-28 19:04:10 +0100

    Updated autoscale log output
    This is updated to work with https://github.com/edwardsp/ood-slurm-autoscale-app

--- azod-slurm/scripts/suspend.sh
+++ azod-slurm/scripts/suspend.sh
@@ -2,13 +2,16 @@
 
 NODES=$1
 
-echo "$(date) : [suspend.sh] : running suspend with options $@" >> /var/log/slurm/autoscale.log
+AZLOG=$(uuidgen)
+
+echo "$(date) # begin suspend # $NODES # $AZLOG" >> /var/log/slurm/autoscale.log
 
 az login --identity
 
 source /apps/slurm/azurehpc/install.sh
 
 cd /apps/slurm/azscale
-azhpc slurm_suspend "$NODES" >> /var/log/slurm/autoscale.log 2>&1
+azhpc slurm_suspend --no-color "$NODES" >> /var/log/slurm/suspend_${AZLOG}.log 2>&1
 
-echo "$(date) : [suspend.sh] : exiting" >> /var/log/slurm/autoscale.log
+echo "$(date) # end suspend # $NODES # $AZLOG" >> /var/log/slurm/autoscale.log
+
